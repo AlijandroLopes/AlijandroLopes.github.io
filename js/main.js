48ec7793bca8a78ff8e8db430d5211119bbc96fc
AlijandroLopes <302735476@student.rocmondriaan.nl>
2023-06-16 00:38:06 +0200

    added buttons

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const buttonLearnMore = document.querySelector('.button-view-more');
+
+buttonLearnMore.addEventListener('click', function() {
+    
+    console.log('button learn more clicked');
+
+   
+
+});
+
+
